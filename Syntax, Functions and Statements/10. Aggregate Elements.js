0b703516ad565a263de8f2257dee6ad13acc5041
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-14 16:48:42 +0300

    Lab js done!

---
+++ Syntax, Functions and Statements/10. Aggregate Elements.js
@@ -0,0 +1,30 @@
+// let aggregateElements = (array) => {
+//     let numbersArray = array.map(Number);
+
+//     let sum = numbersArray.reduce((a, b) => a + b);
+//     let inverseValuesSum = numbersArray.reduce((a, b) => a + 1 / b, 0);    
+//     let stringConcat = numbersArray.join('');
+
+//     console.log(sum);
+//     console.log(inverseValuesSum);
+//     console.log(stringConcat);
+// }
+
+// aggregateElements([2, 4, 8, 16]);
+
+function aggregateElements(input) {
+    let elements = input.map(Number);
+    aggregate(elements, 0, (a, b)=>a + b);
+    aggregate(elements, 0, (a, b)=>a + 1 / b);
+    aggregate(elements, "", (a, b)=>a + b);
+ 
+    function aggregate(arr, initVal, func) {
+        let val = initVal;
+        for (let i = 0; i < arr.length; i++) {
+            val = func(val, arr[i]);
+        }
+        console.log(val);
+    }
+}
+ 
+aggregateElements([1, 2, 3]);
