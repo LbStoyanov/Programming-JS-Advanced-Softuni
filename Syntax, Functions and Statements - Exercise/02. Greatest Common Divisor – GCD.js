5e0945353c1d887f42742d2a1c42927731c134a5
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-15 17:44:40 +0300

    JS

--- Syntax, Functions and Statements - Exercise/02. Greatest Common Divisor – GCD.js
+++ Syntax, Functions and Statements - Exercise/02. Greatest Common Divisor – GCD.js
@@ -1,14 +1,32 @@
-function greatestCommonDivisor(x, y) {
-    if ((typeof x !== 'number') || (typeof y !== 'number')) 
-    return false;
-  x = Math.abs(x);
-  y = Math.abs(y);
-  while(y) {
-    var t = y;
-    y = x % y;
-    x = t;
-  }
-  console.log(x);
+// function greatestCommonDivisor(x, y) {
+//     if ((typeof x !== 'number') || (typeof y !== 'number')) 
+//     return false;
+//   x = Math.abs(x);
+//   y = Math.abs(y);
+//   while(y) {
+//     var t = y;
+//     y = x % y;
+//     x = t;
+//   }
+//   console.log(x);
+// }
+
+// greatestCommonDivisor(100, 40);
+
+// Second Solution
+function solve(a,b){
+    let firstNum = Number(a);
+    let secondNum = Number(b);
+
+    while(firstNum !== secondNum){
+        if (firstNum > secondNum) {
+            firstNum -= secondNum;
+        }else{
+            secondNum -= firstNum;
+        }
+    }
+
+    console.log(firstNum);
 }
 
-greatestCommonDivisor(100, 40);
+solve(15,5)
