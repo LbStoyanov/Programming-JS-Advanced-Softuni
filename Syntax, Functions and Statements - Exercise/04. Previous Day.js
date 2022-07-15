b16e5e61a5eaad46d9e34530ca88c7e593390285
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-15 20:56:51 +0300

    Done!

--- Syntax, Functions and Statements - Exercise/04. Previous Day.js
+++ Syntax, Functions and Statements - Exercise/04. Previous Day.js
@@ -1,9 +1,20 @@
+// function solve(year,month,day){
+//     let nextDay = new Date(year, month - 1, day - 1);
+//     let newYear = nextDay.getFullYear();
+//     let newMonth = nextDay.getMonth() + 1;
+//     let newDate = nextDay.getDate();
+//     console.log(`${newYear}-${newMonth}-${newDate}`);
+// }
+
+// solve(2016, 5, 5); 
+
+//Another Solution
+
 function solve(year,month,day){
-    let nextDay = new Date(year, month - 1, day - 1);
-    let newYear = nextDay.getFullYear();
-    let newMonth = nextDay.getMonth() + 1;
-    let newDate = nextDay.getDate();
-    console.log(`${newYear}-${newMonth}-${newDate}`);
+    let dateInput = `${year}-${month}-${day}`;
+    let date = new Date(dateInput);
+    date.setDate(date.getDate() - 1);
+    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
 }
 
 solve(2016, 5, 5);
