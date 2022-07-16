bc8ba9268bc44a74730c75e1c51c139c07fbc1f0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-16 11:02:02 +0300

    Task 05 JS Advanced

--- Syntax, Functions and Statements - Exercise/05. Time to Walk.js
+++ Syntax, Functions and Statements - Exercise/05. Time to Walk.js
@@ -1,3 +1,15 @@
-function solve(){
-  
+function solve(steps, footLength, speedKmH) {
+    let distance = steps * footLength;
+    let rest = Math.floor(distance / 500) * 60;
+    let speed = (speedKmH * 1000) / 3600;
+    let time = (distance / speed) + rest;
+
+    let hours = Math.floor(time / 3600).toFixed(0).padStart(2,'0');
+    let minutes = Math.floor(time / 60).toFixed(0).padStart(2,'0');
+    let seconds = (time % 60).toFixed(0).padStart(2,'0');
+
+    let format = `${hours}:${minutes}:${seconds}`;
+    console.log(format);
 }
+
+solve(4000, 0.60, 5)
