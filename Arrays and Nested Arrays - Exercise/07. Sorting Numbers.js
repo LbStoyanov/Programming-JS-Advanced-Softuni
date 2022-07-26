eed519e38686042142ed7199e9d95c882e60490c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-26 17:42:38 +0300

    Js-Advanced

--- Arrays and Nested Arrays - Exercise/07. Sorting Numbers.js
+++ Arrays and Nested Arrays - Exercise/07. Sorting Numbers.js
@@ -1,25 +1,37 @@
-function solve(input){
-    
-    input.sort(function(a, b) {
-        return a - b;
-      });
-    
+// function solve(input){
 
-    let result = [];
+//     input.sort(function(a, b) {
+//         return a - b;
+//       });
 
-    let rightCounter = input.length - 1;
+//     let result = [];
 
-    for (let i = 0, len = input.length / 2 ; i <= len; i++) {
-        if (rightCounter == input.length / 2) {
-            break;
-        }
-        result.push(input[i]);
-        result.push(input[rightCounter]);
-        rightCounter--;
-    }
+//     let rightCounter = input.length - 1;
 
-    
-   return result;
+//     for (let i = 0, len = input.length / 2 ; i <= len; i++) {
+//         if (rightCounter == input.length / 2) {
+//             break;
+//         }
+//         result.push(input[i]);
+//         result.push(input[rightCounter]);
+//         rightCounter--;
+//     }
+
+//    return result;
+// }
+
+function solve(array) {
+  let resultArr = [];
+  array = array.sort(function (a, b) {
+    return a - b;
+  });
+
+  while (array.length !== 0) {
+    resultArr.push(array.shift());
+    resultArr.push(array.pop());
+  }
+
+  return resultArr;
 }
 
-console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56,10]));
+console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
