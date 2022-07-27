28255fe246ba667bd86d1163b525683fef352b3e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-27 20:36:54 +0300

    Js

---
+++ Objects and Composition - Lab/05.Assembly Line.js
@@ -0,0 +1,13 @@
+const assemblyLine = createAssemblyLine();
+
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
