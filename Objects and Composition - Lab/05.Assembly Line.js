9b99ec14c0db12915c95cfce59244118334fcd88
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-28 11:52:33 +0300

    AssemblyReference

--- Objects and Composition - Lab/05.Assembly Line.js
+++ Objects and Composition - Lab/05.Assembly Line.js
@@ -1,54 +1,47 @@
+function createAssemblyLine() {
+  return {
+    hasClima: (car) => {
+      car.temp = 21;
+      car.tempSettings = 21;
+      car.adjustTemp = () => {
+        if (car.temp < car.tempSettings) {
+          car.temp++;
+        } else if (car.temp > car.tempSettings) {
+          car.temp--;
+        }
+      };
+    },
+    hasAudio: (car) => {
+      car.currentTrack = { name: "", artist: "" };
+      car.nowPlaying = () => {
+        if (car.currentTrack !== null) {
+          console.log(
+            `Now playing '${car.currentTrack.name}' by ${car.currentTrack.artist}`
+          );
+        }
+      };
+    },
+    hasParktronic: (car) => {
+      car.checkDistance = (distance) => {
+        if (distance < 0.1) {
+          console.log("Beep! Beep! Beep!");
+        } else if (distance >= 0.1 && distance < 0.25) {
+          console.log("Beep! Beep!");
+        } else if (distance >= 0.25 && distance < 0.5) {
+          console.log("Beep!");
+        } else {
+          console.log("");
+        }
+      };
+    },
+  };
+}
 const assemblyLine = createAssemblyLine();
 
 const myCar = {
-    make: 'Toyota',
-    model: 'Avensis',
-
-    hasClima(car){
-        car = {
-            temp: 21,
-            tempSettings: 21,
-            adjustTemp: function(){
-                if (this.temp < this.tempSettings) {
-                    this.temp += 1;
-                }else if (this.temp > this.tempSettings) {
-                    this.temp -=1;
-                }
-            }
-
-        };
-    },
-
-    
-    hasAudio(car){
-        car = {
-            currentTrack: {trackName: null, artist: null},
-            nowPlaying: function(){
-                if (this.currentTrack !== null) {
-                    console.log(`Now playing '${currentTrack.name}' by ${currentTrack.artist}`);
-                } 
-            }    
-        }        
-    },
-
-    hasParktronic(car){
-        car = {
-            checkDistance: function(distance){
-                
-                if (distance < 0.1) {
-                    console.log('Beep! Beep! Beep!');
-                }else if (distance >= 0.1 && distance < 0.25) {
-                    console.log('Beep! Beep!');
-                }else if (distance >= 0.25 && distance < 0.5) {
-                    console.log('Beep!');
-                }else {
-                    console.log('');
-                }
-            }
-        }
-    }
+  make: "Toyota",
+  model: "Avensis",
 };
-
 
 assemblyLine.hasClima(myCar);
 console.log(myCar.temp);
