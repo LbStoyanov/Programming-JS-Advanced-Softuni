7835bbff860d8a1991cab36dea29c28fbb907e23
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-04 12:28:19 +0300

    JsAdvanced

---
+++ Objects and Composition - Exercise/08. Rectangle.js
@@ -0,0 +1,20 @@
+function rectangle(width,height,color){
+    let colorWithCapital = color.charAt(0).toUpperCase() + color.slice(1);;
+    let result = {
+        width: Number(width),
+        height: Number(height),
+        color: colorWithCapital,
+        calcArea: function () {
+            return this.width * this.height;
+        }
+    }
+
+    return result;
+
+}
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
