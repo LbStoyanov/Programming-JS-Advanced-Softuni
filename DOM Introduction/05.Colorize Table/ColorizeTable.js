f46bbe53652191a2845c1e1b54c70685643496b0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-09 11:00:50 +0300

    DOM Introduction - Lab

--- DOM Introduction/05.Colorize Table/ColorizeTable.js
+++ DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,12 @@
 function colorize() {
-    // TODO
+    //let rowElements = document.querySelectorAll('tr:nth-of-type(2n) td');
+    let rowElements = document.getElementsByTagName('tr');
+
+    let rows = Array.from(rowElements);
+
+    rows.forEach((x,i) =>{
+        if (i % 2 != 0) {
+            x.style.backgroundColor = 'teal';
+        }
+    });
 }
