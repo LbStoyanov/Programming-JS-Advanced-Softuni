426393f9e6d3e8c1232ee09c99795c9c51164848
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-09 10:31:21 +0300

    DOM Introduction - Lab

--- DOM Introduction/03.Sum Numbers/SumNumbers.js
+++ DOM Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,15 @@
 function calc() {
     // TODO: sum = num1 + num2
+
+    let firsNumElement = document.getElementById('num1');
+    let secondNumElement = document.getElementById('num2');
+    let result = document.getElementById('sum');
+    let firstNum = Number(firsNumElement.value);
+    let secondNum = Number(secondNumElement.value);
+
+    let sum = firstNum + secondNum;
+
+    result.value = sum;
+
+    //console.log(firstNum + secondNum);
 }
