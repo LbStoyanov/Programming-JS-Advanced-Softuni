5a8558172a748b00b5936c8b4fa594023bc08c0e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-10 15:23:19 +0300

    DOM Introduction - Exercise

--- Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+  let originalText = document.getElementById("text").value;
+  let requestedFormat = document.getElementById("naming-convention").value;
+  let transformation = '';
+
+  if (requestedFormat === "Camel Case") {
+
+    for (var i = 0; i < originalText.length; i++) {
+        if (originalText[i] === ' ') {
+            transformation += originalText[i + 1].toUpperCase();
+            i++;
+        }else{
+            transformation += originalText[i].toLowerCase();
+        }
+    }
+  } else if (requestedFormat === "Pascal Case") {
+    for (var i = 0; i < originalText.length; i++) {
+        if (originalText[i] === ' ') {
+            transformation += originalText[i + 1].toUpperCase();
+            i++;
+        }else{
+            if (i === 0) {
+                transformation += originalText[i].toUpperCase();
+                continue;
+            }
+            transformation += originalText[i].toLowerCase();
+        }
+    }
+    
+  } else {
+    transformation = "Error!";
+  }
+
+  let result = document.getElementById("result");
+  result.textContent = transformation;
 }
