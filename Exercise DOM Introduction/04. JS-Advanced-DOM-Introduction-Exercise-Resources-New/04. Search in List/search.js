2ba56e96bd5fe686c26cdcbafd716fca6895ebc8
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-10 17:26:48 +0300

    DOM Introduction - Exercise

--- Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   let towns = document.querySelectorAll('ul li');
+   let searchedText = document.getElementById('searchText').value;
+ 
+   let townsArr = Array.from(towns);
+   let foundMatches = 0;
+
+   for (let town of towns) {
+      if (town.textContent.includes(searchedText)) {
+         foundMatches++;
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+      }
+      else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+   let result = document.getElementById('result');
+   
+   result.textContent = `${foundMatches} matches found`;
 }
