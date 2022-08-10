3b60175209d545e7a23640e30724363f17da4b4b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-10 14:26:22 +0300

    DOM Introduction - Exercise

--- Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,3 +1,10 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNum = document.getElementById('firstNumber').value;
+    
+    let secondNum = document.getElementById('secondNumber').value;
+    let result = document.getElementById('result');
+    let sum = firstNum - secondNum;
+    
+    result.textContent = sum;
+    console.log(sum);
 }
