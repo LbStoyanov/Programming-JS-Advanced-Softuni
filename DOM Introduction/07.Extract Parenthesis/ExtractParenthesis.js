482d4410cb18744c1537790e381bef718b0097af
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-10 12:23:41 +0300

    DOM Introduction - Lab

--- DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
+  let contentElements = document.getElementById(content);
 
+  let pattern = /\(([^(]+)\)/g;
+  let matches = contentElements.textContent.matchAll(pattern);
+  let result = [];
+  for (const match of matches) {
+    result.push(match[1]);
+  }
+
+  return result.join('; ');
 }
