f0c230c10e2074751288a7db195443f575dfb3fb
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-10 12:03:59 +0300

    DOM Introduction - Lab

--- DOM Introduction/06.Sum Table/SumTable.js
+++ DOM Introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,12 @@
 function sumTable() {
+    let costs = document.querySelectorAll('tr td:nth-of-type(2)');
 
+    let costsArr = Array.from(costs).reduce((a,x) =>{
+        let currentValue = Number(x.textContent) || 0;
+        return a + currentValue;
+    }, 0);
+
+    let result = document.getElementById('sum')
+    result.textContent = costsArr;
+    
 }
