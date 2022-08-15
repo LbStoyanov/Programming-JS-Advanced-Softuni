5cdda1ec331c5d6273f8ee6b69493c4f662c50d2
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-15 14:20:24 +0300

    JsAdvanced

--- Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+    //1.Take the input from the browser.
+    let input = document.getElementById('input');
+    //2.Convert the taken input into splitted by . array.
+    let textArray = input.value.split('.').filter(s => s !== '');
+    let resultDiv = document.getElementById('output');
+
+    while (textArray.length > 0) {
+      let text = textArray.splice(0,3).join('. ') + '.';
+      let p = document.createElement('p');
+      p.textContent = text;
+      resultDiv.appendChild(p);
+    }
+    
 }
