1e099616242fe27ef1b59820ae804d4f76e06cb3
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-17 12:05:44 +0300

    DOM Manipulations

--- DOM Manipulations and Events/01. List-Of-Items/app.js
+++ DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+  
+  let itemsList = document.getElementById("items");
+
+  let newItem = document.createElement("li");
+  itemsList.appendChild(newItem);
+//   newItem.textContent = 'opa';
+//   console.log(itemsList);
+
+  let itemText = document.getElementById("newItemText").value;
+  newItem.textContent = itemText;
+  //itemText.value = ' ';
+  
+
+  //newItem.textContent = itemText.textContent;
 }
