4f3533822138b3f3ded0a1152e55cc5fa8ec0631
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-17 15:48:40 +0300

    DOM Manipulations

--- DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,19 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove',gradientMouseoverHandler);
+    gradientElement.addEventListener('mouseout', gradientOut);
+    
+    function gradientMouseoverHandler(event) {
+        let percentageBar = event.offsetX / (event.target.clientWidth - 1);
+
+        percentageBar = Math.trunc(percentageBar * 100);
+        resultElement.textContent = percentage + "%";
+    };
+ 
+    
+    function gradientOut(event) {
+        resultElement.textContent = '';
+    }
 }
