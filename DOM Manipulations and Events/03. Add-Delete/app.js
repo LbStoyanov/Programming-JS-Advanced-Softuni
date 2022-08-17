f36f41622b1a337fd76ea5ddd639d897fc51886c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-17 15:00:30 +0300

    DOM Manipulations

--- DOM Manipulations and Events/03. Add-Delete/app.js
+++ DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    //TODO...
+    let inputElement = document.getElementById('newItemText').value;
+    let itemElementsList = document.getElementById('items');
+
+    let liElement = document.createElement('li');
+    liElement.textContent = inputElement;
+    itemElementsList.appendChild(liElement);
+
+    let deleteButtonElement = document.createElement('a');
+    deleteButtonElement.href = '#';
+    deleteButtonElement.textContent = '[Delete]';
+    deleteButtonElement.addEventListener('click',  (e) => {
+        e.currentTarget.parentElement.remove();
+    });
+
+    liElement.appendChild(deleteButtonElement);
+    
 }
