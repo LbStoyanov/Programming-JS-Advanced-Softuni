299fb835cf2ef9c0c9ae3e2c953556d9e422794f
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-17 13:03:43 +0300

    DOM Manipulations

--- DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,20 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+
+    let emailInput = document.querySelector('input[name="email"]').value;
+    
+
+    let tableBodyElements = document.querySelectorAll('tr td:nth-of-type(2)');//Select every second td in a tr
+                                                                             //document.querySelectorAll('tbody tr td'); select all td
+
+    let emailArrayElements = Array.from(tableBodyElements);
+    let targetEmail = emailArrayElements.find(x => x.textContent == emailInput);
+    let result = document.getElementById('result');
+
+    if (targetEmail == null) {
+        result.textContent = 'Not found.';
+    }else{
+        targetEmail.parentNode.remove();
+        result.textContent = "Deleted."
+    }
+
 }
