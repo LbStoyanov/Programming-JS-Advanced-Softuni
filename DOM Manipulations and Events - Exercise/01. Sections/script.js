1a1526cd452738b9d5209d3bea979f8bc975e1ba
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-23 12:37:21 +0300

    DOM eventListeners

--- DOM Manipulations and Events - Exercise/01. Sections/script.js
+++ DOM Manipulations and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,24 @@
 function create(words) {
-   console.log('TODO:...');
+   let mainContainer = document.getElementById('content');
+   let input = words;
+
+   for (let i = 0; i < input.length; i++) {
+      
+      let div = document.createElement('div');
+      let paragrah = document.createElement('p');
+      let text = document.createTextNode(input[i]);
+
+      paragrah.appendChild(text);
+      paragrah.style.display = 'none';
+      div.appendChild(paragrah);
+
+      div.addEventListener('click',showParagraphs);
+      mainContainer.appendChild(div);
+
+   }
+
+   function showParagraphs(event){
+      event.target.children[0].style.display = 'inline';
+   }
+
 }
