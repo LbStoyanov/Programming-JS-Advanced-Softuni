a0baacfed6581fbac8b4ae9fb702bdec2a6b58e0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-23 14:08:13 +0300

    DOM Manipulations

--- DOM Manipulations and Events - Exercise/02. Time-Converter/solution.js
+++ DOM Manipulations and Events - Exercise/02. Time-Converter/solution.js
@@ -1,58 +1,75 @@
 function attachEventsListeners() {
   let days = document.getElementById("days");
-  let daysButton = document.getElementById("daysBtn");
+  //let daysButton = document.getElementById("daysBtn");
   let hours = document.getElementById("hours");
-  let hoursButton = document.getElementById("hoursBtn");
+  //let hoursButton = document.getElementById("hoursBtn");
   let minutes = document.getElementById("minutes");
-  let minutesButton = document.getElementById("minutesBtn");
+  //let minutesButton = document.getElementById("minutesBtn");
   let seconds = document.getElementById("seconds");
-  let secondsButton = document.getElementById("secondsBtn");
+  //let secondsButton = document.getElementById("secondsBtn");
 
-  let currentDays = 0;
-  let currentHours = 0;
-  let currentMinutes = 0;
-  let currentSeconds = 0;
 
-  daysButton.addEventListener("click", function (e) {
-    currentDays = days.value;
-    currentHours = currentDays * 24;
-    currentMinutes = currentHours * 60;
-    currentSeconds = currentMinutes * 60;
-    hours.value = currentHours;
-    minutes.value = currentMinutes;
-    seconds.value = currentSeconds;
-  });
+  let conversionObject = {
+        day:1,
+        hour:24,
+        minute:1440,
+        second:86400
+  };
+  
+  document.getElementById("daysBtn").addEventListener('click',onConvert);
+  document.getElementById("hoursBtn").addEventListener('click',onConvert);
+  document.getElementById("minutesBtn").addEventListener('click',onConvert);
+  document.getElementById("secondsBtn").addEventListener('click',onConvert);
 
-  hoursButton.addEventListener("click", function (e) {
-    currentHours = hours.value;
-    currentDays = currentHours / 24;
-    currentMinutes = currentHours * 60;
-    currentSeconds = currentMinutes * 60;
-    days.value = currentDays;
-    minutes.value = currentMinutes;
-    seconds.value = currentSeconds;
-  });
-
-  minutesButton.addEventListener("click", function (e) {
-    currentMinutes = minutes.value;
-    currentHours = currentMinutes / 60;
-    currentDays = currentHours / 24;
-    currentSeconds = currentMinutes * 60;
-    days.value = currentDays;
-    hours.value = currentHours;
-    seconds.value = currentSeconds;
-  });
-
-  secondsButton.addEventListener("click", function (e) {
-    currentSeconds = seconds.value;
-    currentMinutes = currentSeconds / 60;
+  function onConvert(event){
     
-    minutes.value = currentMinutes;
-    currentHours = currentMinutes / 60;
-    hours.value = currentHours;
+  }
 
-    currentDays = currentHours / 24;
-    days.value = currentDays;
+//   let currentDays = 0;
+//   let currentHours = 0;
+//   let currentMinutes = 0;
+//   let currentSeconds = 0;
+
+//   daysButton.addEventListener("click", function (e) {
+//     currentDays = days.value;
+//     currentHours = currentDays * 24;
+//     currentMinutes = currentHours * 60;
+//     currentSeconds = currentMinutes * 60;
+//     hours.value = currentHours;
+//     minutes.value = currentMinutes;
+//     seconds.value = currentSeconds;
+//   });
+
+//   hoursButton.addEventListener("click", function (e) {
+//     currentHours = hours.value;
+//     currentDays = currentHours / 24;
+//     currentMinutes = currentHours * 60;
+//     currentSeconds = currentMinutes * 60;
+//     days.value = currentDays;
+//     minutes.value = currentMinutes;
+//     seconds.value = currentSeconds;
+//   });
+
+//   minutesButton.addEventListener("click", function (e) {
+//     currentMinutes = minutes.value;
+//     currentHours = currentMinutes / 60;
+//     currentDays = currentHours / 24;
+//     currentSeconds = currentMinutes * 60;
+//     days.value = currentDays;
+//     hours.value = currentHours;
+//     seconds.value = currentSeconds;
+//   });
+
+//   secondsButton.addEventListener("click", function (e) {
+//     currentSeconds = seconds.value;
+//     currentMinutes = currentSeconds / 60;
     
-  });
+//     minutes.value = currentMinutes;
+//     currentHours = currentMinutes / 60;
+//     hours.value = currentHours;
+
+//     currentDays = currentHours / 24;
+//     days.value = currentDays;
+    
+//   });
 }
