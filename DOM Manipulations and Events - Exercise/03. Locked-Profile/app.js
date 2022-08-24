f33b46dc7d67f5495ec150ed81cab5e8106765bd
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-24 10:32:17 +0300

    DOM Events

--- DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    console.log('op');
+    Array.from(document.querySelectorAll('.profile button'))
+    .forEach(b => b.addEventListener('click',onclick));
+
+    function onclick(e){
+        let profile = e.target.parentElement;
+        let isActive = profile.querySelector("input[value=unlock]").checked;
+       if(isActive){
+            let info = Array.from(profile.querySelectorAll('div'))
+            .find(p => p.id.includes('HiddenFields'));
+            if (e.target.textContent === 'Show more') {
+                e.target.textContent = 'Hide it';
+                info.style.display = 'block';
+            }else{
+                e.target.textContent = 'Show more';
+                info.style.display = 'none';
+            }
+       }
+    }
 }
