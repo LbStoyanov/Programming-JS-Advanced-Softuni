df7de23cf1501404013a1a173a7b1058d4a0587e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-09-13 10:41:16 +0300

    AdvancedFuncsJs

--- Advanced Functions - Exercise/08. Next Article/script.js
+++ Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,16 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(input) {
+    
+    let articles = input;
+
+    return () =>{
+       
+            if (articles.length > 0) {
+                let contentElement = document.getElementById('content');
+            let article = document.createElement('article');
+            let currentText = articles.shift();
+            article.innerText = currentText;
+            contentElement.appendChild(article);
+            }
+        
+    }
 }
