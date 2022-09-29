d468c15c9800f638acb1cd06622bef6d3adf5200
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-09-29 14:32:55 +0300

    Send

--- Syntax, Functions and Statements - Exercise/08. Validity Checker.js
+++ Syntax, Functions and Statements - Exercise/08. Validity Checker.js
@@ -1,25 +1,32 @@
-function solve(input) {
-    let x1 = Number(input[0]);
-    let y1 = Number(input[1]);
+function solve(arr) {
+    let x1 = Number(arr[0]);
+    let y1 = Number(arr[1]);
+    let x2 = Number(arr[2]);
+    let y2 = Number(arr[3]);
 
-    let x2 = Number(input[2]);
-    let y2 = Number(input[3]);
+    function distance(x1, y1, x2, y2) {
+        let distH = x1 - x2;
+        let distY = y1 - y2;
+        return Math.sqrt(distH**2 + distY**2);
+    }
 
-    console.log(Number.isInteger(result(x1, y1))
-        ? `{${x1}, ${y1}} to {0, 0} is valid`
-        : `{${x1}, ${y1}} to {0, 0} is invalid`);
-    console.log(Number.isInteger(result(x2, y2))
-        ? `{${x2}, ${y2}} to {0, 0} is valid`
-        : `{${x2}, ${y2}} to {0, 0} is invalid`);
+    if (Number.isInteger(distance(x1, y1, 0, 0))) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
 
-    console.log(Number.isInteger(result(x1-x2), (y1-y2))
-        ? `{${x1}, ${y1}} to {${x2}, ${y2}} is valid`
-        : `{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    if (Number.isInteger(distance(x2, y2, 0, 0))) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
 
-
-    function result(x, y) {
-        return Math.sqrt(x ** 2 + y ** 2);
+    if (Number.isInteger(distance(x1, y1, x2, y2))) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
     }
 }
 
-solve([1,2,3,4]);
+solve([2, 1, 1, 1]);
