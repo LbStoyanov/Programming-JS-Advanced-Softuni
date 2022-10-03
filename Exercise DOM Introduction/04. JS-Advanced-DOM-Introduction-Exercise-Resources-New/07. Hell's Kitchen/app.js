4aa146effd7a7c816e718c5d6b3d108978db43b5
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-03 16:32:14 +0300

    JS

--- Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -5,43 +5,69 @@ function solve() {
 
       let inputData = JSON.parse(document.getElementById("inputs").children[1].value);
 
+    
+      let allRestaurants = [];
       
+
       for (let restInfo of inputData) {
             //Mikes - Steve 1000, Ivan 200, Paul 800
             
             let currRest = restInfo.split(' - ');
 
+            
+
             let currRestName = currRest[0];
             //Mikes
             let currentWorkers = currRest[1].split(', ');
+
+            let workersArr = [];
+            let totalSalary = 0;
             
             //Steve 1000, Ivan 200, Paul 800
-            let workers = [];
+            
 
-         for (let i = 0; i < currentWorkers.length; i+=2) {
+         for (let i = 0; i < currentWorkers.length; i++) {
             
             let currentWorkerInfo = currentWorkers[i].split(' ');
 
             let currentWorkerName = currentWorkerInfo[0];
-            let currentWorkerSalary = Number(currentWorkerInfo[i + 1]);
+            let currentWorkerSalary = Number(currentWorkerInfo[1]);
+            totalSalary += currentWorkerSalary;
 
             let worker = {
                workerName: currentWorkerName,
-               workerSalary: currentWorkerSalary
+               workerSalary: currentWorkerSalary,
             }
 
-            workers.push(worker);
-          
+            workersArr.push(worker);
          }
 
          let restaurant = {
             restName: currRestName,
-            workers
+            workersArr,
+            averageSalary: totalSalary / workersArr.length,
+            workersCount: workersArr.length
          }
+
+         if (!allRestaurants.includes(restaurant)) {
+            allRestaurants.push(restaurant);
+         }else {
+            allRestaurants.find(restaurant)
+         }
+         
+
       }
-   
+
+
+
       let bestRestaurant = document.querySelector('#bestRestaurant p');
+      bestRestaurant.textContent =
+       `Name: ${0} Average Salary: ${0} Best Salary: ${0}`;
+
+
+
       let workers = document.querySelector('#workers p');
+      workers.textContent = `Name: ${0} With Salary: ${0}`;
 
      //["Mikes - Steve 1000, Ivan 200, Paul 800","Fleet - Maria 850, Janet 650"]
    
