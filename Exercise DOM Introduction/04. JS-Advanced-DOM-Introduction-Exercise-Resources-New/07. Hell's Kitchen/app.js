d117901d38db7c01069b67a73fa65b5f7ef724e9
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-03 15:06:34 +0300

    JS

--- Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -6,23 +6,37 @@ function solve() {
       let inputData = JSON.parse(document.getElementById("inputs").children[1].value);
 
       
-      for (let rest of inputData) {
+      for (let restInfo of inputData) {
             //Mikes - Steve 1000, Ivan 200, Paul 800
             
-            let currRest = rest.split(' - ');
+            let currRest = restInfo.split(' - ');
+
             let currRestName = currRest[0];
             //Mikes
             let currentWorkers = currRest[1].split(', ');
             
             //Steve 1000, Ivan 200, Paul 800
+            let workers = [];
 
          for (let i = 0; i < currentWorkers.length; i+=2) {
-            //console.log(currRest[i]);
-            let currentWorkerInfo = currentWorkers[i].split(' ');
-            let currentWorkerName = currentWorkerInfo[0];
             
+            let currentWorkerInfo = currentWorkers[i].split(' ');
+
+            let currentWorkerName = currentWorkerInfo[0];
             let currentWorkerSalary = Number(currentWorkerInfo[i + 1]);
+
+            let worker = {
+               workerName: currentWorkerName,
+               workerSalary: currentWorkerSalary
+            }
+
+            workers.push(worker);
           
+         }
+
+         let restaurant = {
+            restName: currRestName,
+            workers
          }
       }
    
