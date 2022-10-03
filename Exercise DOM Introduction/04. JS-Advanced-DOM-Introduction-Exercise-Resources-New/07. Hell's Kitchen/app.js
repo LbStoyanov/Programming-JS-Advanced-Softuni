26c9a1f937cd72548cdf07982f4507fe3d55f2e8
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-03 14:12:48 +0300

    js

--- Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -3,35 +3,16 @@ function solve() {
 
    function onClick () {
 
-      let inputData = document.querySelector('#inputs textarea').value.replaceAll("[", "_");
-      console.log(inputData);
-     
+      let inputData = JSON.parse(document.getElementById("inputs").children[1].value);
 
-   //   for (let el of arr) {
-   //     console.log(el);
-   //     break;
-   //   }
-      // console.log(inputData[0]);
-      // console.log(inputData[1]);
-      // console.log(inputData[2]);
-
+      
+      
+   
       let bestRestaurant = document.querySelector('#bestRestaurant p');
       let workers = document.querySelector('#workers p');
 
-     
-      
-     for (let restaurant of inputData) {
-         //["PizzaHut - Peter 500, George 300, Mark 800","TheLake - Bob 1300,Joe 780, Jane 660"];
-         let restInfo = restaurant.split('-');
-         //console.log(typeof(restInfo));
-        
-        for (let i = 0; i < restInfo.length; i++) {
-            //console.log(restInfo[i]);
-            break;
-        }
-     }
-      
-      
+     //["Mikes - Steve 1000, Ivan 200, Paul800","Fleet - Maria 850, Janet 650"]
+   
    }
 }
 
