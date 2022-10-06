ec5734c2a42054a4107dbbd3431f27ca65afb82b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-06 15:53:14 +0300

    JS EVENTS

--- DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,19 +1,47 @@
 function encodeAndDecodeMessages() {
+
   let buttons = document.querySelectorAll("button");
+  let textAreas = document.getElementsByTagName("textarea");
+  let senderTextArea = textAreas[0];
+  let receiverTextArea = textAreas[1];
+  
   let encodeButton = buttons[0];
   let decodeButton = buttons[1];
 
+  encodeButton.addEventListener("click",encode);
+  decodeButton.addEventListener("click",decode);
 
-//   console.log(encodeButton);
-//   console.log(decodeButton);
-//   debugger;   
-  let mainContainer = document.getElementById("main").children;
+  function encode(e) {
+    let message = e.target.parentElement.children[1].value;
+    let result = [];
+    
+    for(let i =0;i < message.length;i++){
+        let currentAschiValue = Number(message.charCodeAt(i));
+        let newAschiValue = currentAschiValue + 1;
+        let newChar = String.fromCharCode(newAschiValue);
+        result.push(newChar);
+    }
+    
+    senderTextArea.value = '';
+    receiverTextArea.value = result.join("");
 
-  let encodeMessageTextArea = mainContainer[0].children[1];
-  
+  }
 
+  function decode(e) {
+    let message = e.target.parentElement.children[1].value;
+    let result = [];
+    
+    for(let i =0;i < message.length;i++){
+        let currentAschiValue = Number(message.charCodeAt(i));
+        let newAschiValue = currentAschiValue - 1;
+        let newChar = String.fromCharCode(newAschiValue);
+        result.push(newChar);
+    }
+    
+    //senderTextArea.value = '';
+    receiverTextArea.value = result.join("");
+  }
 
-  function encode() {}
-
-  function encode() {}
 }
+
+//The password for my bank account is 123pass321
---
+++ DOM Manipulations and Events - Exercise/test.js
@@ -0,0 +1,8 @@
+let word = 'udri';
+
+for(let i = 0;i < word.length;i++){
+   
+    let currentChar = word.charCodeAt(i);
+    console.log(currentChar);
+     
+}
