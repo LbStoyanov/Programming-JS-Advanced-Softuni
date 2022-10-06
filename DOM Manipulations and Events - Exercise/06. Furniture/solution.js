b4c5a392ac5c29f4e761f3219fe18ff307d67af0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-06 17:58:31 +0300

    DomEvents

--- DOM Manipulations and Events - Exercise/06. Furniture/solution.js
+++ DOM Manipulations and Events - Exercise/06. Furniture/solution.js
@@ -16,37 +16,25 @@ function solve() {
 
     for(let item of currentItems){
       let rowToAdd = document.createElement("tr");
-      rowToAdd.innerHTML = `<td>
-                              <img src=${item.img}>
-                            </td>
-                            <td>
-                              <p>${item.name}</p>
-                            </td>
-                            <td>
-                              <p>${item.price}</p>
-                            </td>
-                            <td>
-                              <p>${item.decFactor}</p>
-                            </td>
-                            <td>
-                              <input type="checkbox">
-                            </td>
-                            `;
-                            
-      //let td = document.createElement("td");
-      //let img = document.createElement("img");
-      // img.setAttribute("src", item.img);
-      // td.appendChild(img);
-      // rowToAdd.appendChild(td);
-      // let townTd = document.createElement("td");
-      // let townName = document.createElement("p");
-      // townName.innerText = item.name;
-      // townTd.appendChild(townName);
-      // rowToAdd.appendChild(townTd);
+      rowToAdd.innerHTML = `<td><img src=${item.img}></td>` +
+                            `<td><p>${item.name}</p></td>` +
+                            `<td><p>${item.price}</p></td>`+
+                            `<td><p>${item.decFactor}</p>` +
+                            `</td><td><input type="checkbox">`;
+
       tableBody.appendChild(rowToAdd);
     }
    
   }
 
-  function buy(e) {}
+  function buy(e) {
+    let resultArea =document.querySelectorAll("textarea")[1];
+    let table = Array.from(document.querySelectorAll("tbody tr"));
+    let result = [];
+    for(let el of table){
+      if(el.querySelector("input[type=checkbox]:checked")){
+        
+      }
+    }
+  }
 }
