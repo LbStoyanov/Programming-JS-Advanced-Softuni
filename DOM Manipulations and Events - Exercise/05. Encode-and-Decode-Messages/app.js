e5d01f62695ee2aac3a5a8af85920eea37fab01e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-06 14:31:56 +0300

    JS Dom

--- DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,19 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  let buttons = document.querySelectorAll("button");
+  let encodeButton = buttons[0];
+  let decodeButton = buttons[1];
+
+
+//   console.log(encodeButton);
+//   console.log(decodeButton);
+//   debugger;   
+  let mainContainer = document.getElementById("main").children;
+
+  let encodeMessageTextArea = mainContainer[0].children[1];
+  
+
+
+  function encode() {}
+
+  function encode() {}
 }
