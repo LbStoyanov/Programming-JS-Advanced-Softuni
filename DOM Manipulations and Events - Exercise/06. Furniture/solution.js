1f46e24ef04ccbdae7cb4152d25a6678cea3c54f
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-07 11:26:52 +0300

    DomManipulations

--- DOM Manipulations and Events - Exercise/06. Furniture/solution.js
+++ DOM Manipulations and Events - Exercise/06. Furniture/solution.js
@@ -33,8 +33,32 @@ function solve() {
     let result = [];
     for(let el of table){
       if(el.querySelector("input[type=checkbox]:checked")){
-        
+        let tableData = Array.from(el.querySelectorAll('td'));
+        let info = {
+          name: tableData[1].children[0].textContent,
+          price:tableData[2].children[0].textContent,
+          decFactor: tableData[3].children[0].textContent,
+        }
+
+        result.push(info);
       }
     }
+
+    let names = '';
+    let totalPrice = 0;
+    let decFactor = 0;
+    for(let i = 0; i < result.length; i++){
+      let item = result[i];
+      let separator = i == result.length - 1 ? '' :', ';
+      names += item.name + separator;
+      totalPrice += Number(item.price);
+      decFactor += Number(item.decFactor);
+    }
+
+    decFactor /= result.length;
+
+    resultArea.value = 
+    `Bought furniture: ${names}\nTotal price: ${totalPrice.toFixed(2)}\nAverage decoration factor: ${decFactor}`;
+
   }
 }
