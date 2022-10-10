bd6a660a15e4459e6698ed72bac6a954aaca0d6b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-10 17:44:14 +0300

    UnitTestAndErrorHandling

---
+++ Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/01. Request Validator.js
@@ -0,0 +1,43 @@
+function requestValidator(obj){
+
+    let possibleMethods = ['GET', 'POST','DELETE','CONNECT'];
+    let patter;
+    let validUris = new RegExp(patter);
+    let possibleVersions = ['HTTP/0.9','HTTP/1.0','HTTP/1.1','HTTP/2.0'];
+
+    let forbidenCharacters = ['>','<','\\','&','\'','"'];
+
+    if(!possibleMethods.includes(obj.method)){
+        throw new Error('Invalid request header: Invalid Method');
+    }
+
+    if(!possibleVersions.includes(obj.version)){
+        throw new Error('Invalid request header: Invalid Version');
+    }
+
+    if(verifyRequestMessage(obj.message)){
+        throw new Error('Invalid request header: Invalid Message');
+    }
+
+    function verifyRequestMessage(message){
+        for(let i = 0; i < message.length; i++){
+            let currentChar = message[i];
+
+            if (forbidenCharacters.includes(currentChar)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    return obj;
+}
+
+requestValidator({
+    method: 'POST',
+    uri: 'svn.public.catalog',
+    version: 'HTTP/1.1',
+    message: '\'; DROP TABLE'
+  }
+  );
