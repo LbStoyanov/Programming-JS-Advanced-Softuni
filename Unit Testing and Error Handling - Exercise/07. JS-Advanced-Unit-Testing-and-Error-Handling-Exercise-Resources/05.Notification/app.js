f84f69b8332efab24ad2ecc4ad67e0543ae9e702
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-11 15:16:38 +0300

    UnitTestingAndDomManipulations

--- Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/05.Notification/app.js
+++ Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/05.Notification/app.js
@@ -1,4 +1,20 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  //let button = document.getElementsByTagName('button')[0];
+
+  let notificationDiv = document.getElementById('notification');
+
+  notificationDiv.textContent = message;
+
+  if (notificationDiv.style.display !== "none") {
+    notificationDiv.style.display = "none";
+  } else {
+    notificationDiv.style.display = "block";
+  }
+
+  
+
+  notificationDiv.addEventListener('click',()=>{
+    notificationDiv.style.display = "none";
+  })
+  
 }
