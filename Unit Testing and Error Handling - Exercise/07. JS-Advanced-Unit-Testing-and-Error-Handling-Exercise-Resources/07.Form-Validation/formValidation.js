76386745fc90a1191333f14c7bad0a14db6c8156
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-12 12:02:05 +0300

    Dom Manipulations

--- Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/07.Form-Validation/formValidation.js
+++ Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/07.Form-Validation/formValidation.js
@@ -1,13 +1,51 @@
 function validate() {
     let inputFields = document.getElementsByTagName('input');
 
-    let usernameInput = inputFields[0].textContent;
-    let email = inputFields[1];
-    let password = inputFields[2];
-    let confirmPass = inputFields[3];
+    let usernameInput = inputFields[0];
+
+    let emailInput = inputFields[1];
+
+    let passwordInput = inputFields[2];
+    let confirmPassInput = inputFields[3];
+
     let isCompanyCheckbox = inputFields[4];
     let companyNumber = inputFields[5];
 
-    console.log(usernameInput);
-    
+    usernameInput.addEventListener('change', (e) =>{
+        let username = e.target.value;
+        let regex = new RegExp('^[a-zA-Z0-9]{3,20}$');
+
+        if (!regex.test(username)) {
+            usernameInput.style.borderColor = 'red';
+        }
+    })
+
+    emailInput.addEventListener('change',  (e) => {
+        let email = e.target.value;
+        let regex = new RegExp('\w*[@].*?[\.]{1,}\w*');
+
+        if (!regex.test(email)) {
+            emailInput.style.borderColor = 'red';
+        }
+    })    
+
+    let password;
+    let confirmedPassword;
+
+    passwordInput.addEventListener('change', (e) =>{
+        password = e.target.value;
+        let regex = new RegExp('^\w{5,15}$');
+
+        if (!regex.test(password)) {
+            passwordInput.style.borderColor = 'red';
+        }
+    })
+
+    confirmPassInput.addEventListener('change',(e) => {
+        confirmedPassword = e.target.value;
+
+        if (!password === confirmedPassword) {
+            confirmPassInput.style.borderColor = 'red';
+        }
+    })
 }
