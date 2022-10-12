d7a3c6630c78bd7b8ca931a972e7b169b86d2004
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-12 12:39:31 +0300

    DomManipulations

--- Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/07.Form-Validation/formValidation.js
+++ Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/07.Form-Validation/formValidation.js
@@ -1,51 +1,60 @@
 function validate() {
-    let inputFields = document.getElementsByTagName('input');
+  let inputFields = document.getElementsByTagName("input");
 
-    let usernameInput = inputFields[0];
+  let usernameInput = inputFields[0];
 
-    let emailInput = inputFields[1];
+  let emailInput = inputFields[1];
 
-    let passwordInput = inputFields[2];
-    let confirmPassInput = inputFields[3];
+  let passwordInput = inputFields[2];
+  let confirmPassInput = inputFields[3];
 
-    let isCompanyCheckbox = inputFields[4];
-    let companyNumber = inputFields[5];
+  let isCompanyCheckbox = inputFields[4];
+  let companyNumber = inputFields[5];
 
-    usernameInput.addEventListener('change', (e) =>{
-        let username = e.target.value;
-        let regex = new RegExp('^[a-zA-Z0-9]{3,20}$');
+  usernameInput.addEventListener("change", (e) => {
+    let username = e.target.value;
+    let regex = new RegExp("^[a-zA-Z0-9]{3,20}$");
 
-        if (!regex.test(username)) {
-            usernameInput.style.borderColor = 'red';
-        }
-    })
+    if (!regex.test(username)) {
+      usernameInput.style.borderColor = "red";
+    } else {
+      usernameInput.style.borderColor = "none";
+    }
+  });
 
-    emailInput.addEventListener('change',  (e) => {
-        let email = e.target.value;
-        let regex = new RegExp('\w*[@].*?[\.]{1,}\w*');
+  emailInput.addEventListener("change", (e) => {
+    let email = e.target.value;
+    let regex = new RegExp("w*[@].*?[.]{1,}w*");
 
-        if (!regex.test(email)) {
-            emailInput.style.borderColor = 'red';
-        }
-    })    
+    if (!regex.test(email)) {
+      emailInput.style.borderColor = "red";
+    } else {
+      emailInput.style.borderColor = "none";
+    }
+  });
 
-    let password;
-    let confirmedPassword;
+  //let password;
+  let confirmedPassword;
 
-    passwordInput.addEventListener('change', (e) =>{
-        password = e.target.value;
-        let regex = new RegExp('^\w{5,15}$');
+  passwordInput.addEventListener("change", (e) => {
+    debugger;
+    let password = e.target.value;
+    let pattern = new RegExp("^w{5,15}$");
+    
+    if (!pattern.test(password)) {
+        console.log('Deba');
+    }else{
+        console.log('ne stava');
+    }
+  });
 
-        if (!regex.test(password)) {
-            passwordInput.style.borderColor = 'red';
-        }
-    })
+  confirmPassInput.addEventListener("change", (e) => {
+    confirmedPassword = e.target.value;
 
-    confirmPassInput.addEventListener('change',(e) => {
-        confirmedPassword = e.target.value;
-
-        if (!password === confirmedPassword) {
-            confirmPassInput.style.borderColor = 'red';
-        }
-    })
+    if (password === confirmedPassword) {
+      confirmPassInput.style.borderColor = "none";
+    } else {
+      confirmPassInput.style.borderColor = "red";
+    }
+  });
 }
