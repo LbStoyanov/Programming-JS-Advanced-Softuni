03b351eb7ca6af38ad22f0b7280b021b298f0942
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-12 10:09:37 +0300

    UnitTestingAndDom

--- Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/07.Form-Validation/formValidation.js
+++ Unit Testing and Error Handling - Exercise/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/07.Form-Validation/formValidation.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    let inputFields = document.getElementsByTagName('input');
+
+    let usernameInput = inputFields[0].textContent;
+    let email = inputFields[1];
+    let password = inputFields[2];
+    let confirmPass = inputFields[3];
+    let isCompanyCheckbox = inputFields[4];
+    let companyNumber = inputFields[5];
+
+    console.log(usernameInput);
+    
 }
