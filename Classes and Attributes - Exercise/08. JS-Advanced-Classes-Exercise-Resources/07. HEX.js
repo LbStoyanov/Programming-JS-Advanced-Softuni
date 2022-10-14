01fe437bcbfb5f67fbc15aab9679d1365e939baa
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-14 16:50:08 +0300

    JsClasses

---
+++ Classes and Attributes - Exercise/08. JS-Advanced-Classes-Exercise-Resources/07. HEX.js
@@ -0,0 +1,13 @@
+class Hex {
+    // TODO: implement this class...
+}
+
+
+let FF = new Hex(255);
+console.log(FF.toString());
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString()==='0xF');
+console.log(FF.parse('AAA'));
