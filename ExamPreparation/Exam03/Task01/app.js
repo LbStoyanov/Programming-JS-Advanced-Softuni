4ecbc124b16c3fb46e913cc2b0794cead691a6e5
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-17 22:02:11 +0300

    ExamPrep

--- ExamPreparation/Exam03/Task01/app.js
+++ ExamPreparation/Exam03/Task01/app.js
@@ -1,21 +1,106 @@
 function solve() {
     document.getElementById('add').addEventListener('click',addToTheList);
+    document.getElementById('reset').addEventListener('click',cleareInputFields);
     let recipientName = document.getElementById('recipientName');
     let title = document.getElementById('title');
     let message = document.getElementById('message');
 
-    function addToTheList(){
+    let recipientValue;
+    let titleValue;
+
+    function addToTheList(e){
+        recipientValue = recipientName.value;
+        titleValue = title.value;
+        e.preventDefault();
+
+        if (!recipientName.value || !title.value || !message.value) {
+            return;
+        }
+
         let ul = document.getElementById('list');
 
         let li = document.createElement('li');
 
         let firsth4 = document.createElement('h4');
         firsth4.textContent = `Title: ${title.value}`;
-        
+
         let secondh4 = document.createElement('h4');
         secondh4.textContent = `Recipient Name: ${recipientName.value}`;
 
+        let span = document.createElement('span');
+        span.textContent = message.value;
 
+        let div = document.createElement('div');
+        div.setAttribute('id','list-action');
+
+        let sendBtn = document.createElement('button');
+        sendBtn.innerText = 'Send';
+        sendBtn.setAttribute('id', 'send');
+        sendBtn.addEventListener('click',send);
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'Delete';
+        deleteBtn.setAttribute('id','delete');
+        deleteBtn.addEventListener('click',deleteEmail);
+
+        div.appendChild(sendBtn);
+        div.appendChild(deleteBtn);
+
+        li.appendChild(firsth4);
+        li.appendChild(secondh4);
+        li.appendChild(span);
+        li.appendChild(div);
+
+        ul.appendChild(li);
+
+        cleareInputFields();
+
+
+    }
+
+    function deleteEmail(){
+        let listOfMails = document.getElementById('list');
+        listOfMails.remove(); 
+    }
+
+    function send(){
+        let ul = document.getElementsByClassName('sent-list')[0];
+
+        let span1 = document.createElement('span');
+        span1.textContent = `To: ${recipientValue}`;
+
+        let span2 = document.createElement('span');
+        span2.textContent = `Title: ${titleValue}`;
+
+
+        let div = document.createElement('div');
+        div.classList.add('btn');
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('delete');
+        deleteBtn.innerText = 'Delete';
+
+        div.appendChild(deleteBtn);
+
+        let li = document.createElement('li');
+
+        li.appendChild(span1);
+        li.appendChild(span2);
+        li.appendChild(div);
+
+        ul.appendChild(li);
+
+        let listOfMails = document.getElementById('list');
+        listOfMails.remove();
+
+    }
+
+    
+
+    function cleareInputFields(){
+        recipientName.value = '';
+        title.value = '';
+        message.value = '';
     }
 }
 solve()
