23d240c89a7736360e9cafa202e5be119d3fb82c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-17 14:35:42 +0300

    ExamPrepJs

--- ExamPreparation/Exam02/Task1/Forum Posts_Ресурси/app.js
+++ ExamPreparation/Exam02/Task1/Forum Posts_Ресурси/app.js
@@ -1,100 +1,71 @@
 window.addEventListener("load", solve);
 
+function solve() {
+  document.getElementById("publish-btn").addEventListener("click", createPost);
+  let postTitle = document.getElementById("post-title");
+  let postCategory = document.getElementById("post-category");
+  let postContent = document.getElementById("post-content");
+  let reviewSection = document.getElementById('review-list');
 
-function solve(){
-    document.getElementById('publish-btn').addEventListener('click',createPost);
+ 
 
-    function createPost(){
-      
+
+
+  function createPost(e) {
+    let titleValue = postTitle.value;
+    let categoryValue = postCategory.value;
+    let contentValue = postContent.value;
+
+    if (!titleValue || !categoryValue || !contentValue) {
+      return;
     }
-}
 
-/* function solve() {
-  let originalTitle = '';
-  let originalCategory = '';
-  let originalContent = '';
-
-  let postTitle = document.getElementById('post-title');
-  let postCategory = document.getElementById('post-category');
-  let postContent = document.getElementById('post-content');
-  
-  let ulElement = document.getElementById('review-list');
-
-  let editBtn = document.createElement('button');
-  let approveBtn = document.createElement('button');
-
-  let publishButton = document.getElementById('publish-btn').addEventListener('click',(e) =>{
-
-    if(postTitle.value !==''){
-    
-      let li = document.createElement('li');
-      let article = document.createElement('article');
-      let h4 = document.createElement('h4');
-      let p1 = document.createElement('p');
-      let p2 = document.createElement('p');
-      h4.textContent = postTitle.value;
-      originalTitle = postTitle.value;
-      originalCategory = postCategory.value;
-      originalContent = postContent.value;
-      p1.textContent = `Category: ${postCategory.value}`;
-      p2.textContent = `Content: ${postContent.value}`;
-    
-      article.appendChild(h4);
-      article.appendChild(p1);
-      article.appendChild(p2);
-
-      li.setAttribute('class','rpost');
-      li.appendChild(article);
-      
-      
-      editBtn.innerText = 'Edit';
-      editBtn.setAttribute('class','action-btn');
-      
-      approveBtn.innerText = 'Approve';
-      approveBtn.setAttribute('class','action-btn');
-
-      
-      li.appendChild(approveBtn);
-      li.appendChild(editBtn);
-
-      ulElement.appendChild(li);
-
-      clearInputFields();
-     
-    }
-  });
-
-  editBtn.addEventListener('click' ,(e) =>{
+    createDOMElements(titleValue,categoryValue,contentValue);
 
 
-    postTitle.value = originalTitle;
-    postCategory.value = originalCategory;
-    postContent.value = originalContent;
-
-    let elementForRemove = document.getElementById('review-list').children[0];
-    ulElement.removeChild(elementForRemove);
-  })
-
-
-  approveBtn.addEventListener('click', (e) =>{
-    let li = document.getElementsByClassName('rpost')[0];
-    let article = li.children[0];
-    let editBtn = li.children[1];
-    let approveBtn = li.children[2];
-    li.removeChild(editBtn);
-    li.removeChild(approveBtn);
-
-    let publishedList = document.getElementById('published-list');
-    publishedList.appendChild(li);
-
-  })
-
-  function clearInputFields(){
-    postTitle.value = '';
-    postCategory.value = '';
-    postContent.value ='';
   }
 
-} */
+  function createDOMElements(titleValue,categoryValue,contentValue){
+    let li = document.createElement('li');
+    li.classList.add('rpost');
 
+    let article = createArticle(titleValue,categoryValue,contentValue);
+
+    let editButton = document.createElement('button');
+    //editButton.setAttribute('class','action-btn');
+    editButton.classList.add('action-btn');
+    editButton.classList.add('edit');
+    editButton.textContent = 'Edit';
+
+
+    
+    let approveButton = document.createElement('button');
+    //editButton.setAttribute('class','action-btn');
+    approveButton.classList.add('action-btn');
+    approveButton.classList.add('approve');
+    approveButton.textContent = 'Approve';
+
+
+    li.appendChild(article);
+    li.appendChild(editButton);
+    li.appendChild(approveButton);
+
+    reviewSection.appendChild(li);
+  }
+
+  function createArticle(titleValue,categoryValue,contentValue){
+    let h4 = document.createElement('h4');
+    h4.textContent = titleValue;
+    let p1 = document.createElement('p');
+    p1.textContent = `Category: ${categoryValue}`;
+    let p2 = document.createElement('p');
+    p2.textContent = `Content: ${contentValue}`;
+    let article = document.createElement('article');
+    article.appendChild(h4);
+    article.appendChild(p1);
+    article.appendChild(p2);
+
+    return article;
+  }
+}
 
