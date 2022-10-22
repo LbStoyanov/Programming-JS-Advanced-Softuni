f75108625b1c2d9c35e2c58224b508c7f1d7a6cb
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-22 13:19:01 +0300

    FinalExam

---
+++ FinalExam/Task3/task3.test.js
@@ -0,0 +1,10 @@
+const cinema = require("./task3");
+const assert = require("chai").assert;
+
+describe("Tests ...", function () {
+  describe("Test ...", function () {
+    it("Should return the movies in the array", function () {
+        //TODO..
+      });
+  });
+});
